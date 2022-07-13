dbd46ef080fdb6c10b76806cf5e775d14a55bce0
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-13 13:13:26 +0500

    Create database, table and write queries for it

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
+
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar,
+    date_of_birth date,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );

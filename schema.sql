fd1c329b32bc06b0c1b9074ca72aba202788dd73
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-13 13:42:08 +0500

    Add length to name varchar

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
-    name varchar,
+    name varchar(100),
     date_of_birth date,
     escape_attempts INT,
     neutered BOOLEAN,

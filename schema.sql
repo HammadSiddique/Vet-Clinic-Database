bda1acadaa998473f98fcc9c5d5fbd70577d52e8
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-16 21:59:39 +0500

    Create vets and joins table

--- schema.sql
+++ schema.sql
@@ -29,3 +29,21 @@ alter table animals add PRIMARY KEY (id);
 alter table animals drop column species;
 alter table animals add species_id INT REFERENCES species(id);
 alter table animals add owner_id INT REFERENCES owners(id);
+
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(100),
+    age INT,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations(
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id)
+);
+
+CREATE TABLE visits(
+    vet_id INT REFERENCES vets(id),
+    animal_id INT REFERENCES animals(id),
+    date_of_visit DATE
+);
